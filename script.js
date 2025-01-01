25510a4a1e6111b01d2ceb0bfbb175b5571184ab
kader009 <kadermollarasel000@gmail.com>
2025-01-01 22:52:54 +0600

    change name for tag

--- script.js
+++ script.js
@@ -1,17 +1,38 @@
-const audioPlayer = document.getElementById('audioPlayer');
-const playButton = document.getElementById('playButton');
-const pauseButton = document.getElementById('pauseButton');
-const restartButton = document.getElementById('restartButton');
+const audioPlayer = document.getElementById("audioPlayer");
+const playButton = document.getElementById("playButton");
+const pauseButton = document.getElementById("pauseButton");
+const restartButton = document.getElementById("restartButton");
+const songButtons = document.querySelectorAll(".songButton");
 
-playButton.addEventListener('click', () => {
-  audioPlayer.play();
+let currentSong = null;
+
+songButtons.forEach(button => {
+  button.addEventListener("click", (event) => {
+    const songSrc = event.target.getAttribute("data-src");
+    audioPlayer.src = songSrc;
+    currentSong = songSrc;
+    audioPlayer.play();
+    playButton.innerHTML = `<i class="fa fa-pause"></i> Pause`;
+  });
 });
 
-pauseButton.addEventListener('click', () => {
+playButton.addEventListener("click", () => {
+  if (audioPlayer.paused) {
+    audioPlayer.play();
+    playButton.innerHTML = `<i class="fa fa-pause"></i> Pause`;
+  } else {
+    audioPlayer.pause();
+    playButton.innerHTML = `<i class="fa fa-play"></i> Play`;
+  }
+});
+
+pauseButton.addEventListener("click", () => {
   audioPlayer.pause();
+  playButton.innerHTML = `<i class="fa fa-play"></i> Play`;
 });
 
-restartButton.addEventListener('click', () => {
+restartButton.addEventListener("click", () => {
   audioPlayer.currentTime = 0;
   audioPlayer.play();
+  playButton.innerHTML = `<i class="fa fa-pause"></i> Pause`;
 });
